d4c78f732fa3904b005db48218a20617f90cf664
Haar-you <zemlya.pole@gmail.com>
2018-04-07 11:45:09 +0900

    スクリプトの改訂

--- fonts/Serif/Traditional/test.js
+++ fonts/Serif/Traditional/test.js
@@ -2,61 +2,41 @@ window.addEventListener("load", function(){
     var div = document.getElementById("bhaataan");
     var text = "";
 
-    text += "<div>";
-    for(var c=0xe000; c<=0xe01d; ++c){	
-	text += String.fromCharCode(c);
-    }
-    text += "</div>";
-    
-    for(var v=0xe020; v<=0xe029; ++v){
-	text += "<div>";
-	for(var c=0xe000; c<=0xe01d; ++c){	
-	    text += String.fromCharCode(c,v);
-	}
-	text += "</div>";
-    }
+    text += `<div>${range(0xe000, 0xe01d).map(c => String.fromCharCode(c)).join("")}</div>`;
 
-    for(var v=0xe020; v<=0xe028; ++v){
-	text += "<div>";
-	for(var c=0xe000; c<=0xe01d; ++c){	
-	    text += String.fromCharCode(c,v,0xe029);
-	}
-	text += "</div>";
-    }
-    text += "<div>";
-    for(var c=0xe000; c<=0xe01d; ++c){	
-	text += String.fromCharCode(c,0xe02f);
-    }
-    text += "</div>";
-    
+    text += range(0xe020, 0xe029).map(v =>
+				      `<div>${range(0xe000, 0xe01d).map(c => String.fromCharCode(c, v)).join("")}</div>`
+				     ).join("");
 
-    text += "<div>";
-    for(var v=0xe020; v<=0xe029; ++v){
-	text += String.fromCharCode(v);
-    }
-    text += "</div>";
+    text += range(0xe020, 0xe028).map(v =>
+				      `<div>${range(0xe000, 0xe01d).map(c => String.fromCharCode(c, v, 0xe029)).join("")}</div>`
+				     ).join("");
     
-    for(var c=0xe000; c<=0xe01d; ++c){
-    	text += "<div>";
-	for(var v=0xe020; v<=0xe029; ++v){
-	    text += String.fromCharCode(c,v);
-	}
-	text += String.fromCharCode(c,0xe02f);
-	for(var v=0xe020; v<=0xe028; ++v){
-	    text += String.fromCharCode(c,v,0xe029);
-	}
-	text += "</div>";
-    }
+    text += `<div>${range(0xe000, 0xe01d).map(x => String.fromCharCode(x, 0xe02f)).join("")}</div>`;
+    
+    text += `<div>${range(0xe020, 0xe029).concat([0xe02f]).map(x => String.fromCharCode(x)).join("")}</div>`;
 
-    text += "<div>";
-    for(var p=0xe02a; p<=0xe02e; ++p){
-	text += String.fromCharCode(p);
-    }
-    text += "</div>";
+    text += range(0xe000, 0xe01d).map(c => 
+				      `<div>${
+(range(0xe020, 0xe029).concat([0xe02f])).map(v => String.fromCharCode(c, v)).concat(
+range(0xe020, 0xe028).map(v => String.fromCharCode(c, v, 0xe029))
+).join("")
+}</div>`
+				     ).join("");
     
-    
+    text += `<div>${range(0xe02a, 0xe02e).map(x => String.fromCharCode(x)).join("")}</div>`;
+
 
     div.innerHTML = text;
 
 });
+
+function range(a, b){
+    var arr = [];
+    for(var i=a; i<=b; ++i){
+	arr.push(i);
+    }
+    return arr;
+}
+
 
